4848eafe63506a031551aa7b5aa8004c72c8af27
andrewsbusby <64098862+andrewsbusby@users.noreply.github.com>
2021-08-18 16:36:30 -0600

    wip

--- index.js
+++ index.js
@@ -23,7 +23,11 @@ return result
  * trimPropertiesMutation({ name: '  jane  ' }) // returns the object mutated in place { name: 'jane' }
  */
 function trimPropertiesMutation(obj) {
-  // ✨ implement
+  
+  for (let prop in obj){
+    obj[prop] = obj[prop].trim()
+  }
+return obj
 }
 
 /**
